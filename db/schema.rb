4cdf8a51d5e62a5c705931fa365c1905681edee5
ibraheem <ibrahinaziz719@gmail.com>
2022-01-14 21:01:58 +0500

    add user_id in todos table & generate users model and set associations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_113_044_923) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2022_01_14_155508) do
 
-  create_table 'todos', force: :cascade do |t|
-    t.string 'title', null: false
-    t.boolean 'done', default: false, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title", null: false
+    t.boolean "done", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,5 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   it { should validate_presence_of(:title) }
-  it { should validate_length_of(:title).is_at_least(3) }
+  it { should validate_length_of(:title).is_at_least(3) } 
+  it { should belong_to(:user) }
 end
