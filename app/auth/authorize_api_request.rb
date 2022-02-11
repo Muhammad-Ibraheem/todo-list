c99f6251d2bad070bad520e83422dc94440b24c3
ibraheem <ibrahinaziz719@gmail.com>
2022-02-11 15:49:33 +0500

    fix schema

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -19,11 +19,6 @@ class AuthorizeApiRequest
   def user
     # check if user is in the database
     # memoize user object
-<<<<<<< HEAD
-
-=======
-    Rails.cache.read("InvalidJWTs-#{decoded_auth_token[:user_id]}")
->>>>>>> main
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
     # handle user not found
   rescue ActiveRecord::RecordNotFound => e
