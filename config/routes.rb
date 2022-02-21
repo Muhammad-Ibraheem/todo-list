fc102eeb71a43636d3e912649503642473aab180
Muhammad ibraheem <ibrahinaziz719@gmail.com>
2022-02-21 11:40:19 +0500

    add routes to namespace for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :todos
+      resources :authentication
     end
   end
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'users#create'
-  delete 'auth/signout', to: 'authentication#destroy'
 end
---
+++ spec/requests/api/authentication_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+RSpec.describe 'api/authentication', type: :request do
+end
